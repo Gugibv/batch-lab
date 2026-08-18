1f2d231d3a6e283d1a192c42db3660030647392b
grey <greyjing.zhou@ncsi.com.cn>
2026-08-18 11:09:38 +0800

    Initial commit

---
+++ src/test/java/com/example/batch_lab/BatchLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batch_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
